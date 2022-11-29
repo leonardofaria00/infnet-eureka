01537f8ddf0e4dfc148d942e094f982b93f6d4e1
Leonardo Faria <leonardo.faria@e-unicred.com.br>
2022-11-28 21:38:56 -0300

    Created EurekaConfiguration

---
+++ src/main/java/br/edu/infnet/eureka/config/EurekaConfiguration.java
@@ -0,0 +1,9 @@
+package br.edu.infnet.eureka.config;
+
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaServer
+public class EurekaConfiguration {
+}
