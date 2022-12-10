e3d70a28f782e51cc9267f586fefc3688d2c3e4e
Leonardo Faria <leonardo.faria@e-unicred.com.br>
2022-12-10 15:16:21 -0300

    Refactored EurekaClient and ConfigServer to Application

--- src/main/java/br/edu/infnet/eureka/infrastructure/eureka/config/EurekaConfiguration.java
+++
@@ -1,9 +0,0 @@
-package br.edu.infnet.eureka.infrastructure.eureka.config;
-
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableEurekaServer
-public class EurekaConfiguration {
-}
